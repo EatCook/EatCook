14f0110f76c8da14b59852b90a0bf3ce2868b096
roargenie <lazyhoper987@gmail.com>
2024-02-06 18:13:35 +0900

    git init

---
+++ EatCook/EatCookApp.swift
@@ -0,0 +1,17 @@
+//
+//  EatCookApp.swift
+//  EatCook
+//
+//  Created by 이명진 on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct EatCookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
