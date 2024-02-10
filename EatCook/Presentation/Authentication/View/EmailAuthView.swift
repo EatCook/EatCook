d9099fd30ed4bf9bbbbfd55d1bcf96a17f6c48e8
HAEUN KIM <hany01.dev@gmail.com>
2024-02-10 23:23:10 +0900

    [feat] 회원가입 디자인 - 이메일 인증

--- EatCook/Presentation/Authentication/View/EmailAuthView.swift
+++ EatCook/Presentation/Authentication/View/EmailAuthView.swift
@@ -8,9 +8,74 @@
 import SwiftUI
 
 struct EmailAuthView: View {
+    @State var email = ""
+    @State var authNumber = ""
+    @State var isAuthRequest = false
+    
     var body: some View {
-        Text("회원가입 로직")
-        .navigationTitle("회원가입")
+        NavigationStack {
+            VStack {
+                Text("이메일을 인증해 주세요")
+                    .font(.title2)
+                    .padding(.vertical, 28)
+                
+                HStack {
+                    TextField("이메일", text: $email)
+                        .padding(EdgeInsets(top: 0, leading: 14, bottom: 0, trailing: 14))
+                        .frame(height: 55)
+                        .background()
+                        .cornerRadius(10)
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("인증요청")
+                            .font(.callout)
+                            .foregroundStyle(.white)
+                    })
+                    .frame(width: 80,height: 55)
+                    .background(Color.bdActive)
+                    .cornerRadius(10)
+                }.padding(.vertical, 14)
+                .padding(.horizontal, 24)
+                
+                HStack {
+                    TextField("인증번호", text: $authNumber)
+                        .padding(EdgeInsets(top: 0, leading: 14, bottom: 0, trailing: 14))
+                        .frame(height: 55)
+                        .background()
+                        .cornerRadius(10)
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("3:00")
+                    })
+                    .frame(width: 80,height: 55)
+                    .background(isAuthRequest ? Color.bdActive : Color.bdInactive)
+                    .cornerRadius(10)
+                }.padding(.bottom, 14)
+                .padding(.horizontal, 24)
+                
+                Button(action: {
+
+                }, label: {
+                    Text("다음")
+                })
+                .frame(maxWidth: .infinity)
+                .frame(height: 55)
+                .background(isAuthRequest ? Color.bdActive : Color.bdInactive)
+                .cornerRadius(10)
+                .padding(.horizontal, 24)
+
+                Spacer()
+            }
+            .padding(.top, 30)
+        
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.bgPrimary)
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("회원가입").bold()
+        }
     }
 }
 
