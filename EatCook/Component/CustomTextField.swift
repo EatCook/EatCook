dbb9655f1ea121bdd0a3a038219f8ba297be10ed
HAEUN KIM <hany01.dev@gmail.com>
2024-02-14 09:55:51 +0900

    Feature/login (#8)
    * [feat] SplashView 구현
    
    * Merge develop
    
    * [feat] LoginView 디자인
    
    * [feat] 이메일 회원가입 화면 전환
    
    * [feat] 회원가입 디자인 - 이메일 인증
    
    * [feat]  Component - CustomTextField 생성
    
    * [feat] 회원가입 - 비밀번호 입력 디자인 / [fix] 회원가입 - 이메일 [다음] 버튼 NavigationLink 설정
    
    * [feat] StartPopupView 디자인 및 화면 전환
    
    * [feat] CreateProfileView 디자인 / [fix] 에셋 이름 변경 popupCheck -> check
    
    * [feat] 회원가입 - 테마 선택 디자인 / [fix] CustomTextField Border 추가
    
    * [fix] 메뉴 테마 선택 이미지 , 타이틀 디자인 세로로 변경
    
    * [feat] 회원가입 - 구성원 질문뷰 디자인

---
+++ EatCook/Component/CustomTextField.swift
@@ -0,0 +1,25 @@
+//
+//  CustomTextField.swift
+//  EatCook
+//
+//  Created by 김하은 on 2/10/24.
+//
+
+import SwiftUI
+
+struct CustomTextField: View {
+    var placeHolder: String
+    @Binding var text: String
+    
+    var body: some View {
+        TextField(placeHolder, text: $text)
+            .padding(EdgeInsets(top: 0, leading: 14, bottom: 0, trailing: 14))
+            .frame(height: 55)
+            .background(Color.white)
+            .cornerRadius(10)
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.bdBorder, lineWidth:1)
+            )
+    }
+}
---
+++ EatCook/Presentation/Profile/View/CreateProfileView.swift
@@ -0,0 +1,65 @@
+//
+//  CreateProfileView.swift
+//  EatCook
+//
+//  Created by 김하은 on 2/12/24.
+//
+
+import SwiftUI
+
+struct CreateProfileView: View {
+    @State var nickname = ""
+    @State var isButtonEnabled = false
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("프로필을 설정해 주세요")
+                    .font(.title2)
+                    .padding(.vertical, 20)
+                
+                ZStack {
+                    Image(.food1)
+                        .resizable()
+                        .frame(width: 120, height: 120)
+                        .cornerRadius(60)
+                        .clipped()
+                        
+                    Button(action: {
+                        
+                    }, label: {
+                        Image(.camera)
+                            .resizable()
+                            .frame(width: 46, height: 46)
+                    }).offset(x: 45, y: 45)
+                }
+                
+                CustomTextField(placeHolder: "2~6자의 한글의 닉네임 (기호 사용 불가)", text: $nickname)
+                    .padding(.horizontal, 24)
+                    .padding(.top, 36)
+                    .padding(.bottom, 14)
+                
+                NavigationLink(destination: FoodThemeView().toolbarRole(.editor)) {
+                    Text("다음")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 55)
+                    .background(isButtonEnabled ? Color.bdActive : Color.bdInactive)
+                    .cornerRadius(10)
+                    .padding(.horizontal, 24)
+                }
+                
+                Spacer()
+            }
+            .padding(.top, 30)
+            
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.bgPrimary)
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("회원가입")
+        }
+    }
+}
+
+#Preview {
+    CreateProfileView()
+}
