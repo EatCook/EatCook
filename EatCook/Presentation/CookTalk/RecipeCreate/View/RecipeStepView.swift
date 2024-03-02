1498f65043d1768f7ab4ff1be3ba39a8eb7d3101
roargenie <lazyhoper987@gmail.com>
2024-03-03 01:06:01 +0900

    [feat] BrandColor 셋팅 및 RecipeCreateView Design

---
+++ EatCook/Presentation/CookTalk/RecipeCreate/View/RecipeStepView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeStepView.swift
+//  EatCook
+//
+//  Created by 이명진 on 3/2/24.
+//
+
+import SwiftUI
+
+struct RecipeStepView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeStepView()
+}
---
+++ EatCook/Presentation/CookTalk/RecipeCreate/View/RecipeTagView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeTagView.swift
+//  EatCook
+//
+//  Created by 이명진 on 3/2/24.
+//
+
+import SwiftUI
+
+struct RecipeTagView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeTagView()
+}
