292e19eb2d8470ed1c732ef2311ee47ff210c1b3
roargenie <lazyhoper987@gmail.com>
2024-07-25 00:29:22 +0900

    [feat] API 정보 세팅

--- EatCook/Network/API/MyPageAPI.swift
+++ EatCook/Network/API/MyPageAPI.swift
@@ -8,52 +8,77 @@
 import Foundation
 
 enum MyPageAPI: EndPoint {
-    case mypage(_ page: Int?)
+    case mypageMyRecipe(_ page: Int?)
     case mypageArchive
     case mypageProfileEdit(_ nickName: String)
+    case mypageProfileImageEdit(_ fileExtension: String)
+    case mypageUserInfo
     
     var path: String {
         switch self {
-        case .mypage:
-            return "/api/v1/mypage"
+        case .mypageMyRecipe:
+            return "/api/v1/mypage/mypage/my-recipe"
         case .mypageArchive:
             return "/api/v1/mypage/archives"
-        case .mypageProfileEdit:
+        case .mypageProfileEdit, .mypageProfileImageEdit:
             return "/api/v1/mypage/profile"
+        case .mypageUserInfo:
+            return "/api/v1/mypage/user-info"
         }
     }
     
     var httpMethod: HTTPMethod {
         switch self {
-        case .mypage:
+        case .mypageMyRecipe:
             return .get
         case .mypageArchive:
             return .get
         case .mypageProfileEdit:
             return .patch
+        case .mypageProfileImageEdit:
+            return .post
+        case .mypageUserInfo:
+            return .get
         }
     }
     
     var httpTask: HTTPTask {
         switch self {
-        case .mypage(let page):
+        case .mypageMyRecipe(let page):
             return .requestWithParameters(
                 parameters: ["page": page ?? 0],
                 encoding: .urlEncoding
             )
         case .mypageArchive:
-            return .requestWithParameters(parameters: [:], encoding: .urlEncoding)
+            return .requestWithParameters(
+                parameters: [:],
+                encoding: .urlEncoding
+            )
         case .mypageProfileEdit(let nickName):
             return .requestWithParameters(
                 parameters: ["nickName": nickName],
                 encoding: .jsonEncoding
+            )
+        case .mypageProfileImageEdit(let fileExtension):
+            return .requestWithParameters(
+                parameters: ["fileExtension": fileExtension],
+                encoding: .jsonEncoding
+            )
+        case .mypageUserInfo:
+            return .requestWithParameters(
+                parameters: [:],
+                encoding: .urlEncoding
             )
         }
     }
     
     var headers: [String : String]? {
         switch self {
-        case .mypage, .mypageArchive, .mypageProfileEdit:
+        case .mypageMyRecipe,
+                .mypageArchive,
+                .mypageProfileEdit,
+                .mypageProfileImageEdit,
+                .mypageUserInfo:
             return HTTPHeaderField.default
         }
     }
---
+++ EatCook/Network/API/MyPageSettingAPI.swift
@@ -0,0 +1,85 @@
+//
+//  MyPageSettingAPI.swift
+//  EatCook
+//
+//  Created by 이명진 on 7/25/24.
+//
+
+import Foundation
+
+enum MyPageSettingAPI: EndPoint {
+    case leaveEatCook
+    case updatePassword
+    case alarm
+    case updateAlarm
+    case interestCook
+    case updateInterestCook
+    
+    var path: String {
+        switch self {
+        case .leaveEatCook:
+            return "/api/v1/mypage/profile/leave"
+        case .updatePassword:
+            return "/api/v1/mypage/profile/password"
+        case .alarm, .updateAlarm:
+            return "/api/v1/mypage/setting"
+        case .interestCook, .updateInterestCook:
+            return "/api/v1/mypage/setting/interest-cook"
+        }
+    }
+    
+    var httpMethod: HTTPMethod {
+        switch self {
+        case .leaveEatCook:
+            return .delete
+        case .alarm, .interestCook:
+            return .get
+        case .updatePassword, .updateAlarm, .updateInterestCook:
+            return .patch
+        }
+    }
+    
+    var httpTask: HTTPTask {
+        switch self {
+        case .leaveEatCook:
+            return .requestWithParameters(
+                parameters: [:],
+                encoding: .urlEncoding
+            )
+        case .updatePassword:
+            return .requestWithParameters(
+                parameters: [:],
+                encoding: .jsonEncoding
+            )
+        case .alarm:
+            return .requestWithParameters(
+                parameters: [:],
+                encoding: .urlEncoding
+            )
+        case .updateAlarm:
+            return .requestWithParameters(
+                parameters: [:],
+                encoding: .jsonEncoding
+            )
+        case .interestCook:
+            return .requestWithParameters(
+                parameters: [:],
+                encoding: .urlEncoding
+            )
+        case .updateInterestCook:
+            return .requestWithParameters(
+                parameters: [:],
+                encoding: .jsonEncoding
+            )
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case .leaveEatCook, .updatePassword, .alarm, .updateAlarm, .interestCook, .updateInterestCook:
+            return HTTPHeaderField.default
+            
+        }
+    }
+    
+}
