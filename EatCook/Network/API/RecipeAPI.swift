1f01e19f87db3372db11859f2073c18cb97313d7
roargenie <lazyhoper987@gmail.com>
2024-08-15 15:38:07 +0900

    [fix] path 수정

--- EatCook/Network/API/RecipeAPI.swift
+++ EatCook/Network/API/RecipeAPI.swift
@@ -10,52 +10,56 @@ import Foundation
 enum RecipeAPI: EndPoint {
     case recipeRead(_ recipeId: Int)
     case recipeCreate(_ query: RecipeCreateRequestDTO)
-    case recipeUpdate(_ query: RecipeCreateRequestDTO)
-    case recipeDelete(_ postId: Int)
+    case recipeUpdate(_ query: RecipeCreateRequestDTO, _ recipeId: Int)
+    case recipeDelete(_ recipeId: Int)
     
     var path: String {
         switch self {
-        case .recipeCreate:
-            return "/api/v1/recipe/create"
-        case .recipeUpdate:
-            return "/api/v1/recipe/update"
-        case .recipeDelete:
-            return "/api/v1/recipe/delete"
         case .recipeRead(let recipeId):
+            return "/api/v1/recipes/\(recipeId)"
+        case .recipeCreate:
+            return "/api/v1/recipes"
+        case .recipeUpdate(_, let recipeId):
+            return "/api/v1/recipes/\(recipeId)"
+        case .recipeDelete(let recipeId):
             return "/api/v1/recipes/\(recipeId)"
         }
     }
     
     var httpMethod: HTTPMethod {
         switch self {
-        case .recipeCreate, .recipeUpdate, .recipeDelete:
-            return .post
         case .recipeRead:
             return .get
+        case .recipeCreate:
+            return .post
+        case .recipeUpdate:
+            return .patch
+        case .recipeDelete:
+            return .delete
         }
     }
     
     var httpTask: HTTPTask {
         switch self {
+        case .recipeRead(let recipeId):
+            return .requestWithParameters(
+                parameters: ["recipeId": recipeId],
+                encoding: .urlEncoding)
         case .recipeCreate(let query):
             return .requestWithParameters(
                 parameters: query.toDictionary,
                 encoding: .jsonEncoding
             )
-        case .recipeUpdate(let query):
+        case .recipeUpdate(let query, _):
             return .requestWithParameters(
                 parameters: query.toDictionary,
                 encoding: .jsonEncoding
             )
-        case .recipeDelete(let postId):
-            return .requestWithParameters(
-                parameters: ["postId": postId],
-                encoding: .jsonEncoding
-            )
-        case .recipeRead(let recipeId):
+        case .recipeDelete(let recipeId):
             return .requestWithParameters(
                 parameters: ["recipeId": recipeId],
-                encoding: .urlEncoding)
+                encoding: .jsonEncoding
+            )
         }
     }
     
